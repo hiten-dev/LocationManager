87b85243970933a7cea528cd66eff91ba72d1016
unknown <hitenthedj@gmail.com>
2020-05-18 11:57:25 +0530

    init

---
+++ LocationManager/Controllers/ApiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LocationManager.Controllers
+{
+    public class ApiController : Controller
+    {
+        // GET: Api
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string SaveLocation(string locationEntry)
+        {
+            return "Done";
+        }
+    }
+}
---
+++ LocationManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LocationManager.MvcApplication" Language="C#" %>
